a264ac6c3c346a6771fd0eab624b01b1b0ca7a27
KAlexus <karalin.alex@gmail.com>
2015-12-02 15:49:33 +0200

    More purified code

--- RoundHex.cs
+++ RoundHex.cs
@@ -3,23 +3,23 @@ using System.Collections;
 
 public class RoundHex : MonoBehaviour
 {
-    public int HexSize;
-    public int sizeX;
-    public int sizeY;
+    public int hexSize;         //Диаметр гексагональной карты
+    public float floorLevel;    //Уровень плоскости гексагональной карты (Y-coordinates in Unity)
+    private int sizeX;
+    private int sizeY;
     
     void Start()
     {
-        sizeX = HexSize * 2 + 1;                     // Необходимо для вычисления правильного наложения UV, при конвертации
-        sizeY = Mathf.CeilToInt(sizeX * 0.75f);      // квадратной текстуры в гексагональный вид. ВЫСОТА - 0,75 х ШИРИНЫ
-
-        HexMap(HexSize, 0);
+        HexMap(hexSize, floorLevel);
         Camera camera = Camera.main;
-        camera.transform.position = new Vector3(HexSize, 10, HexSize * 0.75f); // Устанавливаем камеру по центру гексагональной сетки
+        camera.transform.position = new Vector3(hexSize, 10, hexSize * 0.75f); // Устанавливаем камеру по центру гексагональной сетки
 
     }
 
-    void HexMap(int size, int z)
+    void HexMap(int size, float floor)
     {
+        sizeX = size * 2 + 1;                       // Необходимо для вычисления правильного наложения UV, при конвертации
+        sizeY = Mathf.CeilToInt(sizeX * 0.75f);     // квадратной текстуры в гексагональный вид. ВЫСОТА - 0,75 х ШИРИНЫ
 
         int numHex = size * 2 + 1;                  // Вычисляем кол-во гексов,
         for (int i=0; i < size; i++)                // где size - диаметр гексагональной карты.
@@ -44,140 +44,58 @@ public class RoundHex : MonoBehaviour
         int num_triangles = 0;
         bool notOdd;
 
-        for (int ly=0; ly < size * 2 + 1; ly++)         // Запускаем цикл постройки гексагональной карты по Y-Высоте
+        for (int ly=0; ly < size * 2 + 1; ly++)     // Запускаем цикл постройки гексагональной карты по Y-Высоте
         {
-            int _dx = Mathf.Abs(size - ly) / 2; ;       // координаты центра первого гекса в ряду
-            int _dy = size * 2 - (ly + 3) % 2 - _dx;    // координаты центра последнего гекса в ряду (для нечетных значений size)
+            // Координаты центра первого и последнего гекса в ряду
+            int _hexFirst = Mathf.Abs(size - ly) / 2;   
+            int _hexLast = (size % 2 == 0) ? size * 2 - (ly + 2) % 2 - _hexFirst : size * 2 - (ly + 3) % 2 - _hexFirst; 
 
-            if (size % 2 == 0)                          // Если размер кратный 2 ( 1 - центральный ряд - 1)
-            {               
-                _dy = size * 2 - (ly + 2) % 2 - _dx;    // Вычисляем координаты центра последнего гекса вряду (тек.ряд+2)
-            }
-
-            for (int lx=_dx; lx < _dy + 1; lx++)        //запускаем цикл постройки по X-Ширине
+            for (int lx= _hexFirst; lx < _hexLast + 1; lx++)    //запускаем цикл постройки по X-Ширине
             {
                 num_Hex++; //Count of сurrent HEX
 
-                #region Vertices and UV
-                //determine if we are in an odd row; if so we need to offset the hexagons
+                #region Vertices, Normals and UV                
 
-                notOdd = ((ly % 2) == 0);
-                if (size % 2 != 0)
+                notOdd = ((ly % 2) == 0);   //determine if we are in an odd row; if so we need to offset the hexagons
+
+                if (size % 2 != 0)          //Если размерность не кратна 2, порядок рядов меняется на противоположный
                 {
                     notOdd = !notOdd;
-                }                
-
-                //Можно уменьшить кол-во строк, если сделать:
-                //
-                //  if (notOdd != true) {__dx=0.50f; __dy=0.75f; __ly=ly-1;}
-                //  else {__dx=0; __dy=0; __ly=ly;}
-                //
-                //и запись вершинт в следующем виде:
-                //
-                //  vertices[0 + num_verts] = new Vector3(0.0f + __dx + lx, z, 0.00f + __dy + 0.75f * __ly);
-                //
-                if (notOdd == true)
-                {
-                    //            /*0*/new Vector3( 0.50f, z,  0.50f),         1
-                    //            /*1*/new Vector3( 0.50f, z,  1.00f),       /   \
-                    //            /*2*/new Vector3( 1.00f, z,  0.75f),      6     2
-                    //            /*3*/new Vector3( 1.00f, z,  0.25f),      |  0  |
-                    //            /*4*/new Vector3( 0.50f, z,  0.00f),      5     3
-                    //            /*5*/new Vector3( 0.00f, z,  0.25f),       \   /
-                    //            /*6*/new Vector3( 0.00f, z,  0.75f)          4    
-                            
-                    //generate the hexagons in the normal positioning for this row
-                    /*0*/
-                    vertices[0 + num_verts] =   new Vector3( 0.50f + lx,            z,  0.50f + 0.75f * ly);
-                    uv[0 + num_verts] =         new Vector2((0.50f + lx) / sizeX,      (0.50f + 0.75f * ly) / sizeY);
-
-                    /*1*/
-                    vertices[1 + num_verts] =   new Vector3( 0.50f + lx,            z,  1.00f + 0.75f * ly);
-                    uv[1 + num_verts] =         new Vector2((0.50f + lx) / sizeX,      (1.00f + 0.75f * ly) / sizeY);
-
-                    /*2*/
-                    vertices[2 + num_verts] =   new Vector3( 1.00f + lx,            z,  0.75f + 0.75f * ly);
-                    uv[2 + num_verts] =         new Vector2((1.00f + lx) / sizeX,      (0.75f + 0.75f * ly) / sizeY);
-
-                    /*3*/
-                    vertices[3 + num_verts] =   new Vector3( 1.00f + lx,            z,  0.25f + 0.75f * ly);
-                    uv[3 + num_verts] =         new Vector2((1.00f + lx) / sizeX,      (0.25f + 0.75f * ly) / sizeY);
-
-                    /*4*/
-                    vertices[4 + num_verts] =   new Vector3( 0.50f + lx,            z,  0.00f + 0.75f * ly);
-                    uv[4 + num_verts] =         new Vector2((0.50f + lx) / sizeX,      (0.00f + 0.75f * ly) / sizeY);
-
-                    /*5*/
-                    vertices[5 + num_verts] =   new Vector3( 0.00f + lx,            z,  0.25f + 0.75f * ly);
-                    uv[5 + num_verts] =         new Vector2((0.00f + lx ) / sizeX,     (0.25f + 0.75f * ly) / sizeY);
-
-                    /*6*/
-                    vertices[6 + num_verts] =   new Vector3( 0.00f + lx,            z,  0.75f + 0.75f * ly);
-                    uv[6 + num_verts] =         new Vector2((0.00f + lx) / sizeX,      (0.75f + 0.75f * ly) / sizeY);
-                }
-                else
-                {
-
-                    //generate the hexagons in the offset positioning for this row
-                    /*0*/
-                    vertices[0 + num_verts] =   new Vector3( 1.00f + lx,            z,  1.25f + 0.75f * (ly - 1));
-                    uv[0 + num_verts] =         new Vector2((1.00f + lx ) / sizeX,     (1.25f + 0.75f * (ly - 1)) / sizeY);
-
-                    /*1*/
-                    vertices[1 + num_verts] =   new Vector3( 1.00f + lx,            z,  1.75f + 0.75f * (ly - 1));
-                    uv[1 + num_verts] =         new Vector2((1.00f + lx) / sizeX,      (1.75f + 0.75f * (ly - 1)) / sizeY);
-
-                    /*2*/
-                    vertices[2 + num_verts] =   new Vector3( 1.50f + lx,            z,  1.50f + 0.75f * (ly - 1));
-                    uv[2 + num_verts] =         new Vector2((1.50f + lx) / sizeX,      (1.50f + 0.75f * (ly - 1)) / sizeY);
-
-                    /*3*/
-                    vertices[3 + num_verts] =   new Vector3( 1.50f + lx,            z,  1.00f + 0.75f * (ly - 1));
-                    uv[3 + num_verts] =         new Vector2((1.50f + lx) / sizeX,      (1.00f + 0.75f * (ly - 1)) / sizeY);
-
-                    /*4*/
-                    vertices[4 + num_verts] =   new Vector3( 1.00f + lx,            z,  0.75f + 0.75f * (ly - 1));
-                    uv[4 + num_verts] =         new Vector2((1.00f + lx ) / sizeX,     (0.75f + 0.75f * (ly - 1)) / sizeY);
-
-                    /*5*/
-                    vertices[5 + num_verts] =   new Vector3( 0.50f + lx,            z,  1.00f + 0.75f * (ly - 1));
-                    uv[5 + num_verts] =         new Vector2((0.50f + lx) / sizeX,      (1.00f + 0.75f * (ly - 1)) / sizeY);
-
-                    /*6*/
-                    vertices[6 + num_verts] =   new Vector3( 0.50f + lx,            z,  1.50f + 0.75f * (ly - 1));
-                    uv[6 + num_verts] =         new Vector2((0.50f + lx) / sizeX,      (1.50f + 0.75f * (ly - 1)) / sizeY);
                 }
 
-                #endregion
+                float _dx, _dy; //Для расчета смещения гекса в зависимости от четности ряда
 
-                #region Normals //equal Vertices count
+                if (notOdd == true) { _dx = lx; _dy = 0.75f * ly; }
+                else { _dx = 0.50f + lx; _dy = 0.75f + 0.75f * (ly-1); }
+
+                Vector3[] _vertices = CalculateVert(_dx, _dy, floor);
+                Vector2[] _uv = CalculateUV(_dx, _dy, floor, sizeX, sizeY);
+
                 for (int i = 0; i < 7; i++)
                 {
+                    vertices[i + num_verts] = _vertices[i];
+                    uv[i + num_verts] = _uv[i];
                     normals[i + num_verts] = Vector3.up;
+
+                    if (i < 6)
+                    {
+                        triangles[0 + i * 3 + num_triangles] = 0 + num_verts;
+                        triangles[1 + i * 3 + num_triangles] = 1 + i + num_verts;
+
+                        if (i == 5)
+                        {
+                            triangles[2 + i * 3 + num_triangles] = -4 + i + num_verts;
+                        }
+                        else
+                        {
+                            triangles[2 + i * 3 + num_triangles] = 2 + i + num_verts;
+                        }
+                    }
                 }
                 #endregion
 
-                #region Triangles
-
-                for (int i = 0; i < 6; i++)
-                {
-                    triangles[0 + i * 3 + num_triangles] = 0 + num_verts;
-                    triangles[1 + i * 3 + num_triangles] = 1 + i + num_verts;
-
-                    if (i == 5)
-                    {
-                        triangles[2 + i * 3 + num_triangles] = -4 + i + num_verts;
-                    }
-                    else
-                    {
-                        triangles[2 + i * 3 + num_triangles] = 2 + i + num_verts;
-                    }
-                }
-
-                #endregion
-
-                num_verts += 7;         //Save count of Vertices after loop
-                num_triangles += 18;    //Save count of Triangles after loop
+                num_verts += 7;         //Save count of Vertices after loop current row
+                num_triangles += 18;    //Save count of Triangles after loop current row
             }
         }
         
@@ -189,24 +107,38 @@ public class RoundHex : MonoBehaviour
 
         MeshFilter mesh_filter = GetComponent<MeshFilter>();
         mesh_filter.mesh = __mesh;
-        mesh_filter.name = "Hex Map";
+        mesh_filter.name = "Round Hex Map";
         mesh_filter.mesh.name = "Hex";
     }
 
-    //Vector3[] CalculateVert(float x, float y, float z, int __sizeX, int __sizeY)  //x,y - center of Hex
-    //{
-    //    Vector3[] __vert = new Vector3[7];
+    Vector3[] CalculateVert(float x, float y, float z)  //x,y - center of Hex
+    {
+        Vector3[] _vertices = new Vector3[7];
 
-    //    //generate the hexagons in the normal positioning for this row
-    //    /*0*/__vert[0] = new Vector3( (0.50f + x) / __sizeX, z, 0.00f + y);
-    //    /*1*/__vert[1] = new Vector3( (0.00f + x) / __sizeX, z, 0.25f + y);
-    //    /*2*/__vert[2] = new Vector3( (0.00f + x) / __sizeX, z, 0.75f + y);
-    //    /*3*/__vert[3] = new Vector3( (0.50f + x) / __sizeX, z, 1.00f + y);
-    //    /*4*/__vert[4] = new Vector3( (1.00f + x) / __sizeX, z, 0.75f + y);
-    //    /*5*/__vert[5] = new Vector3( (1.00f + x) / __sizeX, z, 0.25f + y);
-    //    /*6*/__vert[6] = new Vector3( (0.50f + x) / __sizeX, z, 0.00f + y);
-
-    //    return __vert; 
-    //}
-
+        /*0*/   _vertices[0] = new Vector3( (0.50f + x), z, (0.50f + y) );
+        /*1*/   _vertices[1] = new Vector3( (0.50f + x), z, (1.00f + y) );
+        /*2*/   _vertices[2] = new Vector3( (1.00f + x), z, (0.75f + y) );
+        /*3*/   _vertices[3] = new Vector3( (1.00f + x), z, (0.25f + y) );
+        /*4*/   _vertices[4] = new Vector3( (0.50f + x), z, (0.00f + y) );
+        /*5*/   _vertices[5] = new Vector3( (0.00f + x), z, (0.25f + y) );
+        /*6*/   _vertices[6] = new Vector3( (0.00f + x), z, (0.75f + y) );
+    
+        return _vertices; 
     }
+
+    Vector2[] CalculateUV(float x, float y, float z, int _sizeX, int _sizeY)  //x,y - center of Hex
+    {
+        Vector2[] _uv = new Vector2[7];
+
+        /*0*/   _uv[0] = new Vector2( (0.50f + x) / _sizeX, (0.50f + y) / _sizeY);
+        /*1*/   _uv[1] = new Vector2( (0.50f + x) / _sizeX, (1.00f + y) / _sizeY);
+        /*2*/   _uv[2] = new Vector2( (1.00f + x) / _sizeX, (0.75f + y) / _sizeY);
+        /*3*/   _uv[3] = new Vector2( (1.00f + x) / _sizeX, (0.25f + y) / _sizeY);
+        /*4*/   _uv[4] = new Vector2( (0.50f + x) / _sizeX, (0.00f + y) / _sizeY);
+        /*5*/   _uv[5] = new Vector2( (0.00f + x) / _sizeX, (0.25f + y) / _sizeY);
+        /*6*/   _uv[6] = new Vector2( (0.00f + x) / _sizeX, (0.75f + y) / _sizeY);
+
+        return _uv; 
+    }
+
+}
