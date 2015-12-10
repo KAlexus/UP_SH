a3f59dbe6c7e10c8e5f54dbf73767efdb9e63362
KAlexus <karalin.alex@gmail.com>
2015-12-10 14:19:29 +0200

    worked, but not cleared code

--- HexMapMouse.cs
+++ HexMapMouse.cs
@@ -3,14 +3,12 @@ using System.Collections;
 
 public class HexMapMouse : MonoBehaviour {
     public Vector2 currentSelectedHex;
-    public HexGenerator hexGenerator;
-    int hexSize;
-    //public Camera camera;
-
+    public int hexSize;
     // Use this for initialization
     void Start () {
 
-        hexSize = 2*2;
+        hexSize = 2;
+
     }
 	
 	// Update is called once per frame
@@ -18,13 +16,9 @@ public class HexMapMouse : MonoBehaviour {
         RaycastHit hit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        if (Physics.Raycast(ray, out hit, Mathf.Infinity)) //&& Input.GetKeyDown(KeyCode.Mouse0)
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity) && Input.GetKeyDown(KeyCode.Mouse0)) //
         {
             
-            //int size = hexGenerator.hexSize * 2;
-            //currentSelectedHex = _PixelToHex(hit.point, hexSize);
-            ///Debug.Log("Current HEX[" + hit.point.x + "," + hit.point.z + "]");
-
             MeshCollider meshCollider = hit.collider as MeshCollider;
             if (meshCollider == null || meshCollider.sharedMesh == null)
                 return;
@@ -34,7 +28,7 @@ public class HexMapMouse : MonoBehaviour {
             int[] triangles = mesh.triangles;
             int hitTriangle = (int)(hit.triangleIndex/6);
 
-            Debug.Log("hitTriangle" + hitTriangle);
+            //Debug.Log("hitTriangle" + hitTriangle);
 
             Vector3 p0 = vertices[hitTriangle*6 + 0+ hitTriangle];
             Vector3 p1 = vertices[hitTriangle*6 + 1 + hitTriangle];
@@ -43,19 +37,46 @@ public class HexMapMouse : MonoBehaviour {
             Vector3 p4 = vertices[hitTriangle*6 + 4 + hitTriangle];
             Vector3 p5 = vertices[hitTriangle*6 + 5 + hitTriangle];
             Vector3 p6 = vertices[hitTriangle*6 + 6 + hitTriangle];
-            Debug.Log("Point 0 - " + (hitTriangle * 6 + 0));
-            Debug.Log("Point 1 - " + (hitTriangle*6 + 1) + ", Point 2 - " + (hitTriangle*6 + 2) + ", Point 3 - " + (hitTriangle*6 + 3));
-            Debug.Log("Point 4 - " + (hitTriangle * 6 + 4) + ", Point 5 - " + (hitTriangle * 6 + 5) + ", Point 6 - " + (hitTriangle * 6 + 6));
+            //Debug.Log("Point 0 - " + (hitTriangle * 6 + 0));
+            //Debug.Log("Point 1 - " + (hitTriangle*6 + 1) + ", Point 2 - " + (hitTriangle*6 + 2) + ", Point 3 - " + (hitTriangle*6 + 3));
+            //Debug.Log("Point 4 - " + (hitTriangle * 6 + 4) + ", Point 5 - " + (hitTriangle * 6 + 5) + ", Point 6 - " + (hitTriangle * 6 + 6));
             Transform hitTransform = hit.collider.transform;
 
-           // p0 = hitTransform.TransformPoint(p0);
-           // p1 = hitTransform.TransformPoint(p1);
-           // p2 = hitTransform.TransformPoint(p2);
-           // p3 = hitTransform.TransformPoint(p3);
-            //p4 = hitTransform.TransformPoint(p4);
-            //p5 = hitTransform.TransformPoint(p5);
-            //p6 = hitTransform.TransformPoint(p6);
+            bool oddSize = ((hexSize + 2) % 2==0) ? true : false;
+            
+            if (oddSize)
+            {
+                //Debug.Log("oddSize(true)=" + oddSize);
+                if ((Mathf.FloorToInt(p0.z / 0.75f) + 2) % 2 == 0)
+                {
+                    //Debug.Log("ODD ROW!!!");
+                    currentSelectedHex = new Vector2(Mathf.FloorToInt(p0.z / 0.75f), Mathf.Floor(p0.x));
+                }
+                else
+                {
+                    //Debug.Log("NOT ODD ROW!!!");
+                    currentSelectedHex = new Vector2(Mathf.FloorToInt(p0.z / 0.75f), Mathf.Floor(p0.x) - 1);
+                }
+                    
 
+            }
+            else
+            {
+                //Debug.Log("oddSize(false)=" + oddSize);
+                if ((Mathf.FloorToInt(p0.z / 0.75f) + 2) % 2 == 0)
+                {
+                    //Debug.Log("ODD ROW!!!");
+                    currentSelectedHex = new Vector2(Mathf.FloorToInt(p0.z / 0.75f), Mathf.Floor(p0.x) - 1);
+                }
+                else
+                {
+                    //Debug.Log("NOT ODD ROW!!!");
+                    currentSelectedHex = new Vector2(Mathf.FloorToInt(p0.z / 0.75f), Mathf.Floor(p0.x));
+                }
+                
+            }
+            
+            Debug.Log("HEX[" + currentSelectedHex.x + "," + currentSelectedHex.y + "]");
 
             Debug.DrawLine(p1, p2);
             Debug.DrawLine(p2, p3);
